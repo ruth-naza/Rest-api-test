03e82f0c1dfe320e81640440993cca121b60962d
Ruth Agbasimalo <aruth71@gmail.com>
2019-08-01 05:34:26 +0100

    Fixing nodemon issue

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
 
 
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.info(`Server has started on ${PORT}`)
